9ad59cfeddc607d5354478e58eb1e925d170842c
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-04-27 22:03:06 -0400

    added almost all tests for stacks

--- tests.cpp
+++ tests.cpp
@@ -1,2 +1,78 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "Stacks.h"
+#include "Queues.h"
+
+TEST_CASE("constructor")
+{
+	StackList *s = new StackList();
+	CHECK(s->gethead() == nullptr);
+
+	Queue *q = new Queue();
+
+}
+
+TEST_CASE("deconstructor")
+{
+	StackList *s = new StackList();
+	s->push("0");
+	s->push("1");
+	delete s;		 // cannot reference this anymore
+	s = nullptr; // already deleted so it is good practice to point to nullptr
+	CHECK(s == nullptr);
+}
+
+TEST_CASE("push and enqueue")
+{
+	StackList *s = new StackList();
+	s->push("A");
+	s->push("A");
+	s->push("A");
+	CHECK(s->toString() == "head-->A-->A-->A-->nullptr");
+
+	Queue *q = new Queue();
+	q->enqueue("A");
+	q->enqueue("B");
+	q->enqueue("C");
+	CHECK(q->front() == "A");
+}
+
+TEST_CASE("pop and dequeue")
+{
+	StackList *s = new StackList();
+	s->push("A");
+	s->push("B");
+	s->push("C");
+	CHECK(s->pop() == "B");
+
+	Queue *q = new Queue();
+	q->enqueue("A");
+	q->enqueue("B");
+	q->enqueue("C");
+	CHECK(q->dequeue() == "B"); 
+}
+
+TEST_CASE("top and front"){
+	StackList *s = new StackList();
+	s->push("A");
+	s->push("B");
+	s->push("C");
+	CHECK(s->top() == "C");
+
+	Queue *q = new Queue();
+	q->enqueue("A");
+	q->enqueue("B");
+	q->enqueue("C");
+	CHECK(q->front() == "A");
+	q->dequeue();
+	CHECK(q->front() == "B");
+}
+
+TEST_CASE("is_empty and is_full"){
+	StackList *s = new StackList();
+	CHECK(s->is_empty() == 1);
+
+	Queue *q = new Queue();
+	CHECK(q->is_empty() == true);
+	CHECK(q->is_full() == false);
+}
