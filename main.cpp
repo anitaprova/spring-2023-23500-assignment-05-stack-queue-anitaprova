ea4c4201b2f92dca5233d53afcee7af02b929b2e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-04-27 22:03:40 -0400

    saving progress

--- main.cpp
+++ main.cpp
@@ -11,19 +11,28 @@ int main()
 	s->push("1");
 	s->push("2");
 	s->push("3");
-	std::cout << s->top() << "\n";
-	std::cout << s->is_empty() << "\n";
+	std::cout << "top: " << s->top() << "\n"; // 3
+	std::cout << "empty?: " << s->is_empty() << "\n";
 	std::cout << s->toString() << "\n";
-	s->pop();
+	std::cout << "pop: " << s->pop() << "\n";
 	std::cout << s->toString() << "\n";
 
 	StackList *empty = new StackList;
-	std::cout << empty->is_empty() << "\n";
-	std::string str = "";
+	std::cout << empty->toString() << "\n";
+	std::cout << "empty?: " << empty->is_empty() << "\n";
+
 	try
 	{
-		str = s->pop();
-		std::cout << str << " " << s->top() << "\n";
+		std::cout << empty->top() << "\n";
+	}
+	catch (int e)
+	{
+		std::cout << "stack empty error code: " << e << "\n";
+	}
+
+	try
+	{
+		std::cout << empty->pop() << "\n";
 	}
 	catch (int e)
 	{
@@ -31,15 +40,23 @@ int main()
 	}
 
 	// Part 2
-	std::cout << "PART 2"
-						<< "\n\n\n";
+	std::cout << "\n\n\nPART 2\n";
 	Queue *q = new Queue;
 	q->enqueue("1");
 	q->enqueue("2");
 	q->enqueue("3");
+	q->enqueue("4");
+	q->enqueue("5");
+	//q->enqueue("6");
 	std::cout << q->toString() << "\n";
+	std::cout << "front: " << q->front() << "\n";
 
-	std::cout << q->front() << "\n";
+	std::cout << q->dequeue() << "\n";
+	std::cout << q->dequeue() << "\n";
+	std::cout << q->dequeue() << "\n";
+	std::cout << q->dequeue() << "\n";
+	//std::cout << q->dequeue() << "\n";
+	std::cout << "front: " << q->front() << "\n";
 
 	return 0;
 }
